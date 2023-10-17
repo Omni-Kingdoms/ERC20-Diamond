744fbb75a96b153ed57133b05de5731079fd2fba
Gaurang Torvekar <gaurangtorvekar@gmail.com>
2023-10-17 18:40:15 +0100

    Removed the condition of owner for transfer

--- contracts/facets/ERC20Facet.sol
+++ contracts/facets/ERC20Facet.sol
@@ -4,8 +4,4 @@ pragma solidity ^0.8.0;
 import {LibDiamond} from "../libraries/LibDiamond.sol";
 import {ERC20} from "../solidstate-solidity/token/ERC20/ERC20.sol";
 
-contract ERC20Facet is ERC20 {
-    function _beforeTokenTransfer(address, address, uint256) internal view override {
-        LibDiamond.enforceIsContractOwner();
-    }
-}
+contract ERC20Facet is ERC20 {}

fe43fa133823c57599defc9cda5f540baf54a5cf
Gaurang Torvekar <gaurangtorvekar@gmail.com>
2023-10-17 18:05:41 +0100

    chore: Removed minter role for testing

--- contracts/solidstate-solidity/token/ERC20/base/ERC20Base.sol
+++ contracts/solidstate-solidity/token/ERC20/base/ERC20Base.sol
@@ -2,9 +2,9 @@
 
 pragma solidity ^0.8.0;
 
-import { IERC20 } from '../IERC20.sol';
-import { ERC20BaseInternal } from './ERC20BaseInternal.sol';
-import { ERC20BaseStorage } from './ERC20BaseStorage.sol';
+import {IERC20} from "../IERC20.sol";
+import {ERC20BaseInternal} from "./ERC20BaseInternal.sol";
+import {ERC20BaseStorage} from "./ERC20BaseStorage.sol";
 import {LibDiamond} from "../../../../libraries/LibDiamond.sol";
 
 /**
@@ -22,24 +22,18 @@ abstract contract ERC20Base is IERC20, ERC20BaseInternal {
         return ERC20BaseStorage.layout().maxSupply;
     }
 
-    function feeRecipientAddress() public view virtual returns (address){
+    function feeRecipientAddress() public view virtual returns (address) {
         return ERC20BaseStorage.layout().feeRecipient;
     }
 
-    function minterRole() public view virtual returns (address){
+    function minterRole() public view virtual returns (address) {
         return ERC20BaseStorage.layout().minter;
     }
 
     /**
      * @inheritdoc IERC20
      */
-    function balanceOf(address account)
-        public
-        view
-        virtual
-        override
-        returns (uint256)
-    {
+    function balanceOf(address account) public view virtual override returns (uint256) {
         return _balanceOf(account);
     }
 
@@ -54,32 +48,21 @@ abstract contract ERC20Base is IERC20, ERC20BaseInternal {
     }
 
     function mint(address account, uint256 amount) public {
-        _isAllowedToMint();
+        // _isAllowedToMint();
         _mint(account, amount);
     }
 
     /**
      * @inheritdoc IERC20
      */
-    function allowance(address holder, address spender)
-        public
-        view
-        virtual
-        override
-        returns (uint256)
-    {
+    function allowance(address holder, address spender) public view virtual override returns (uint256) {
         return ERC20BaseStorage.layout().allowances[holder][spender];
     }
 
     /**
      * @inheritdoc IERC20
      */
-    function approve(address spender, uint256 amount)
-        public
-        virtual
-        override
-        returns (bool)
-    {
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
         _approve(msg.sender, spender, amount);
         return true;
     }
@@ -87,12 +70,7 @@ abstract contract ERC20Base is IERC20, ERC20BaseInternal {
     /**
      * @inheritdoc IERC20
      */
-    function transfer(address recipient, uint256 amount)
-        public
-        virtual
-        override
-        returns (bool)
-    {
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
         _transfer(msg.sender, recipient, amount);
         return true;
     }
@@ -100,18 +78,9 @@ abstract contract ERC20Base is IERC20, ERC20BaseInternal {
     /**
      * @inheritdoc IERC20
      */
-    function transferFrom(
-        address holder,
-        address recipient,
-        uint256 amount
-    ) public virtual override returns (bool) {
-        uint256 currentAllowance = ERC20BaseStorage.layout().allowances[holder][
-            msg.sender
-        ];
-        require(
-            currentAllowance >= amount,
-            'ERC20: transfer amount exceeds allowance'
-        );
+    function transferFrom(address holder, address recipient, uint256 amount) public virtual override returns (bool) {
+        uint256 currentAllowance = ERC20BaseStorage.layout().allowances[holder][msg.sender];
+        require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
         unchecked {
             _approve(holder, msg.sender, currentAllowance - amount);
         }
